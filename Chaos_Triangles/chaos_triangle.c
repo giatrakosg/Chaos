6792dcb1bfd245bb5735cc01aa1c7cd94f343cb1
giatrakosg <sdi1600036@di.uoa.gr>
2017-05-01 16:08:16 +0300

    Added option to determine duration of simulation on screen

--- Chaos_Triangles/chaos_triangle.c
+++ Chaos_Triangles/chaos_triangle.c
@@ -3,15 +3,21 @@
 #include <graphics.h>
 #include <unistd.h>
 #include "utils.h"
+#include <time.h>
+
 #define MAX_LOOPS 30000
 #define OFFSET 200
-
+#define SIM_TIME 30 /* Time in seconds that the simulation will appear on screen */
 
 int main(int argc ,char *argv[]) {
     int gd = DETECT,gm;
     int i ,k ;
     struct point p1 , p2 ,p3 ;
     struct point center ;
+    const int TIME_PER_LOOP = (SIM_TIME * 10000000) / (MAX_LOOPS / 100) ;
+    struct timespec req;
+    req.tv_nsec = TIME_PER_LOOP ;
+    req.tv_sec = 0 ;
     initgraph(&gd, &gm, "");
     center.x = getmaxx() / 2 ;
     center.y = getmaxy() / 2 ;
@@ -64,7 +70,7 @@ int main(int argc ,char *argv[]) {
             get_middle(&start,p3);
         }
         putpixel(start.x,start.y,BLUE);
-        //sleep(1);
+        nanosleep(&req,NULL);
     }
 
     getchar();
