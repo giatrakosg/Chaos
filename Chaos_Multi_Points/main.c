db2b6786274b0170e4b44bde8c30590214a3582c
giatrakosg <sdi1600036@di.uoa.gr>
2017-05-03 23:23:59 +0300

    Removed unused variables ,printed circle in center of screen

--- Chaos_Multi_Points/main.c
+++ Chaos_Multi_Points/main.c
@@ -11,15 +11,13 @@
 
 int main(int argc ,char *argv[]) {
 
-    int i ,k ;
-
     struct point center ;
 
-    const int TIME_PER_LOOP = (SIM_TIME * 10000000) / (MAX_LOOPS / 100) ;
+/*    const int TIME_PER_LOOP = (SIM_TIME * 10000000) / (MAX_LOOPS / 100) ;
     struct timespec req;
     req.tv_nsec = TIME_PER_LOOP ;
     req.tv_sec = 0 ;
-
+*/
     int points ;
 
     printf("Give number of points \n" );
@@ -34,11 +32,7 @@ int main(int argc ,char *argv[]) {
     print_shape(mat,points);
     center.x = getmaxx() / 2 ;
     center.y = getmaxy() / 2 ;
-
-    struct point start ;
-
-
-
+    circle(center.x,center.y,100);
 
     getchar();
     closegraph();
